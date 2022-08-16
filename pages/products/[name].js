f769df0b81b7c0c93ab1c3f5671a18ff63328145
Alisson Roger Barbosa Lima <50409048+alisson-roger@users.noreply.github.com>
2022-08-16 11:06:33 -0300

    pages

---
+++ pages/products/[name].js
@@ -0,0 +1,48 @@
+import styles from '../../styles/Product.module.css'
+import { data } from '../../data'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Product = ({ product }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.cardL}>
+        {product.images.map(img => (
+          <div key={img.id} className={styles.imgContainer}>
+            <Image src={img.url} width='100%' height='100%' objectFit='cover' alt='' layout='responsive' />
+          </div>
+        ))}
+      </div>
+      <div className={styles.cardS}>
+        <h1 className={styles.title}>{product.title}</h1>
+        <p className={styles.desc}>{product.desc}</p>
+        <button className={styles.button}>
+          <Link href='/contact'>Contact</Link>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export const getStaticPaths = async () => {
+  const products = data;
+  const paths = products.map((item) => {
+    return {
+      params: { name: item.name },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+export const getStaticProps = async (ctx) => {
+  const name = ctx.params.name;
+  const product = data.filter(item => item.name === name)[0]
+  return {
+    props:
+      { product }
+  }
+}
+
+export default Product
