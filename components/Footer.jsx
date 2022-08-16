fa269335cc764feccd42d4d657869178cbb57984
Alisson Roger Barbosa Lima <50409048+alisson-roger@users.noreply.github.com>
2022-08-16 10:19:56 -0300

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '../styles/footer.module.css';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.cardL}>
+        <h1 className={styles.title}>AV0CAD0 CREATIVES</h1>
+        <h1 className={styles.linkTitle}>
+          <a href="/contact" passHref />
+          <span className={styles.linkText}>WORK WITH US</span>
+          <Image src="/img/link.png" alt="" width="40px" height="40px" />
+        </h1>
+      </div>
+      <div className={styles.cardS}>
+        <div className={styles.cardItem}>
+          12 ADAM STREET NY <br />
+          USA
+        </div>
+        <div className={styles.cardItem}>
+          CONTACT@GMAIL.COM <br />
+          +55 45 12345 6789
+        </div>
+      </div>
+      <div className={styles.cadS}>
+        <div className={styles.cardItem}>
+          12 ADAM STREET NY <br />
+          USA
+        </div>
+        <div className={styles.cardItem}>
+          CONTACT@GMAIL.COM <br />
+          +55 45 12345 6789
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -6,7 +6,7 @@ import { users } from '../data';
 const Testimonials = () => {
   return (
     <div className={style.container}>
-      <Circle top="-70vh" left="0" right="0" backgroundColor="darkblue" />
+      <Circle top="50vh" left="250px" right="0" backgroundColor="darkblue" />
       <h1 className={style.title}>Testimonials</h1>
       <div className={style.wrapper}>
         {users.map((user) => (
